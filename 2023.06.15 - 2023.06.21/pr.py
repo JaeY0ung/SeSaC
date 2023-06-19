7bbc03bca3259ca39718c166ef56233cec91b83e
JaeY0ungJung <hanol5216@gmail.com>
2023-06-19 11:38:18 +0900

    first commit

---
+++ 2023.06.15 - 2023.06.21/pr.py
@@ -0,0 +1,7 @@
+def gugudan():
+    for i in range(2,10):
+        print("%d단"%i)
+        for j in range(1,10):
+            print("%d x %d = %d" %(i,j,i*j))
+
+gugudan()
---
+++ test.py
@@ -0,0 +1,7 @@
+def get_name_age():
+    return "John", 25
+name, age = get_name_age()
+print(name)
+print(age)
+
+# 리스트
