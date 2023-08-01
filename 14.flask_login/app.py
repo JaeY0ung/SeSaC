69c5b1182518dc0f4dcfb18050b89438b224eca2
JaeY0ungJung <hanol5216@gmail.com>
2023-08-01 11:27:13 +0900

    마이그레이션 실습

--- 14.flask_login/app.py
+++ 14.flask_login/app.py
@@ -12,7 +12,8 @@ from flask_migrate import Migrate
 #?             이 함수를 호출하면 세션에 사용자 정보가 저장되어 사용자를 인증된 상태로 유지할 수 있습니다.
 #! login_required: 데코레이터로, 특정 뷰 함수를 보호하는 데 사용됩니다. 
 #!                 이 데코레이터가 적용된 뷰 함수는 로그인된 사용자만 접근할 수 있으며, 로그인되지 않은 사용자는 로그인 페이지로 리디렉션됩니다.
-#? logout_user: 로그아웃 처리를 위해 사용되는 함수로, 현재 로그인된 사용자를 로그아웃 상태로 만듭니다. 세션에서 사용자 정보를 삭제하여 인증을 끝내는 역할을 합니다.
+#? logout_user: 로그아웃 처리를 위해 사용되는 함수로, 현재 로그인된 사용자를 로그아웃 상태로 만듭니다.
+#?              세션에서 사용자 정보를 삭제하여 인증을 끝내는 역할을 합니다.
 #! current_user: 현재 로그인된 사용자를 나타내는 객체입니다. 로그인되지 않은 경우 AnonymousUserMixin 객체가 반환됩니다.
 #!               이를 통해 로그인된 사용자의 정보를 뷰 함수에서 간단하게 접근할 수 있습니다.
 
@@ -35,18 +36,18 @@ login_manager.login_view = "login" # 로그인 페이지 URI 명시
 
 class User(UserMixin, db.Model):
     id            = db.Column(db.Integer,     primary_key = True)
-    username      = db.Column(db.String(80),  unique = True, nullable = False)
-    password      = db.Column(db.String(120),                nullable = False)
-    email         = db.Column(db.String(80), nullable = True)
-    # email         = db.Column(db.String(80),  unique = True, nullable = True)
+    username      = db.Column(db.String(80),  nullable = False)
+    password      = db.Column(db.String(120), nullable = False)
+    password_hash = db.Column(db.String(120), nullable = False)
+    email         = db.Column(db.String(80),  nullable = True)
     
     def set_password(self, password):
-        self.password = password
-        # self.password_hash = generate_password_hash(password)
+        # self.password = password
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return self.password == password
-        # return check_password_hash(self.password_hash, password)
+        # return self.password == password
+        return check_password_hash(self.password_hash, password)
 
 @login_manager.user_loader
 def load_user(user_id):
